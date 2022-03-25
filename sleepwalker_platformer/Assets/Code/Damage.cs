c3655cd2cec71116470c2d1dbf4810174dc82f5b
Amy <ab9316@nyu.edu>
2022-03-25 14:28:37 -0400

    Enemy ANims

--- sleepwalker_platformer/Assets/Code/Damage.cs
+++ sleepwalker_platformer/Assets/Code/Damage.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
+    Animator _animator;
+    
+    private void Start() {
+        _animator = GetComponent<Animator>();
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
@@ -13,7 +18,7 @@ public class Damage : MonoBehaviour
         if(other.CompareTag("Bullet"))
         {
             Destroy(other.gameObject);
-            //_animator.SetTrigger("Die");
+            _animator.SetTrigger("Die");
             Destroy(gameObject);
         }
         
